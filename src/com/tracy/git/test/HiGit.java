35db9e5f914ab68d7eeedc1b255d762fdd74b638
tracywen9326 <tracywen9326@163.c0m>
2019-03-06 20:16:47 +0800

    test

---
+++ src/com/tracy/git/test/HiGit.java
@@ -0,0 +1,11 @@
+package com.tracy.git.test;/*
+ @author tracyWen
+ @create 2019/3/6 000619:51
+ @Version 1.0
+*/
+
+public class HiGit {
+    public static void main(String[] args) {
+        System.out.println("Hi Git!!!");
+    }
+}
